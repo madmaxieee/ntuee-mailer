4efbc9d71f0d3e0e9d5a7820c0646bc2787ee249
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2022-12-03 14:46:06 +0800

    v2.0.8 fix config file init error

--- ntuee_mailer/globals.py
+++ ntuee_mailer/globals.py
@@ -6,6 +6,8 @@ from pathlib import Path
 APP_NAME = "ntuee-mailer"
 APP_ROOT = Path(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))))
 APP_DIR = Path(typer.get_app_dir(APP_NAME))
+# make APP_DIR if it doesn't exist
+APP_DIR.mkdir(parents=True, exist_ok=True)
 CONFIG_PATH = Path(APP_DIR) / "config.ini"
 if not CONFIG_PATH.is_file():
     shutil.copy(APP_ROOT / "config-default.ini", CONFIG_PATH)
